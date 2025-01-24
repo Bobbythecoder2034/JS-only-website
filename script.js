c443229200f957bf4e66a4c2c2381b3ebc7752b7
Bobbythecoder2034 <kkoala110@gmail.com>
2025-01-24 16:54:32 -0700

    completed

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
-// https://i.redd.it/2tuay0z91uyc1.jpeg 
-// https://m.media-amazon.com/images/M/MV5BYmU5ZmE3ODUtNGI0NS00MGYxLWE1NmUtMDgyMDM4NGM2OTA2XkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg
-// https://upload.wikimedia.org/wikipedia/en/e/e7/Helldivers2cover.png
-// https://cdn.britannica.com/67/183767-050-23D72392/World-Data-Locator-Map-Sweden.jpg
+// https://4kwallpapers.com/images/wallpapers/minecraft-8k-video-7680x4320-16364.png
+// https://m.media-amazon.com/images/I/810E0pAjpiL._AC_UF1000,1000_QL80_.jpg
+// https://https://upload.wikimedia.org/wikipedia/en/b/b6/Minecraft_2024_cover_art.png
+// https://i.scdn.co/image/ab67616d0000b273ded18b66b44176024247bcfd
 const siteData = {
     header: "Welcome to Arrowhead Studios",
     bannerImage:  
     {
-        image:"https://i.redd.it/2tuay0z91uyc1.jpeg",
+        image:"https://4kwallpapers.com/images/wallpapers/minecraft-8k-video-7680x4320-16364.png",
         width: "300px",
         height: "150px",
     },
     navLinks:["Home", "About", "Gallery"],
     mainContent:[
         {
-            title: "Helldivers 2",
-            image: "https://upload.wikimedia.org/wikipedia/en/e/e7/Helldivers2cover.png",
-            description: "Our biggest game yet, with the largest audience we have ever seen."
+            title: "Minecraft",
+            image: "https://upload.wikimedia.org/wikipedia/en/b/b6/Minecraft_2024_cover_art.png",
+            description: "Released May 17, 2009, Minecraft instantly took off, selling countless copies on desktops. When it was released on Xbox 360, millions of copies were sold on the first day. It has remained relevant in the gaming industry for all these years. It was also the first game to win game of the year without any other awards."
         },{
-            title: "Helldivers",
-            image: "https://m.media-amazon.com/images/M/MV5BYmU5ZmE3ODUtNGI0NS00MGYxLWE1NmUtMDgyMDM4NGM2OTA2XkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg" ,
-            description: "Our first game was when we really became well known and popular."
+            title: "Minecraft Dungeons",
+            image: "https://m.media-amazon.com/images/I/810E0pAjpiL._AC_UF1000,1000_QL80_.jpg" ,
+            description: "Minecraft Dungeons was released on May 26, 2020. It was a dungeon crawler filled with creative variations of the vanilla creatures and weapons. The game received several DLCs and was very popular for a time. It didn't become as timeless as its predecessor, but it is a good game."
         
         },{
-            title: "Sweden",
-            image: "https://cdn.britannica.com/67/183767-050-23D72392/World-Data-Locator-Map-Sweden.jpg" ,
-            description: "We are based in Sweden",
+            title: "Minecraft Legends",
+            image: "https://i.scdn.co/image/ab67616d0000b273ded18b66b44176024247bcfd" ,
+            description: "Minecraft Legends, released April 18, 2023, was another game that focused on the combat side of Minecraft, but it became a real-time strategy. The creatures of the overworld united against the Nether, forcing the player to act the part of a general in a war between dimensions.",
         }
     ],
     footer:"Arrowhead Studios © 2025. All rights reserved"
@@ -80,30 +80,31 @@ siteData.mainContent.forEach(section =>{
     createAndAppend("img", article, {src:section.image, alt:section.title})
     createAndAppend("p",article,{},section.description)
 });
-const words = "a"
+const words = "Minecraft is the most successful games ever, with over 300 million downloads across the world. Its a sandbox world where anything can be created, with more possibilites being added through regular updates and mods from the community. It has also been reimagined into two more games since Microsoft purchased Mojang from Notch, expanding its horizons. Neither of these games were as popular as the original, which stands the test of time as an enjoyable game for all ages."
 const summary = createAndAppend("p", document.body, {class:"site-summary"},words)
 const footer = createAndAppend("footer", document.body, {class:"site-footer"})
 createAndAppend("p", footer,{},siteData.footer)
 
+
 //Create some Styling
 const style = `
-body{font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif; margin:0; padding:1px; line-height:1.6; background-color:#fff;}
-header{ background-color:#333; color:#fff; padding:1rem; text-align:center;}
-.site-banner img{width:50%; display:block; margin-left:25%;}
+body{font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif; margin:0; padding:1px; line-height:1.6; background-color:#53a334;}
+header{ background-color:#800E3D; padding:1rem; text-align:center;}
+.site-banner img{width:40%; display:block; margin-left:30%;}
 
 nav ul{list-style:none; padding:12px; margin:0; display:flex; justify-content:space-evenly;
-background-color:#555;}
+background-color:#800E3D;}
 nav ul li{margin: 0 1rem;}
 nav ul li a{color: #fff; text-decoration:none;}
-a:hover{color:#9AE85A;}
+a:hover{color:#53a334;}
 h2{display: block;}
 main{padding:1rem; display:flex; flex-direction:row; justify-content:space-evenly;}
 .site-gallery{ display:flex; flex-direction:row;}
-.content-section {margin-bottom:2rem; display:flex; margin:10px; flex-direction:column; align-items:center; width:32%; border-radius:6px; background-color:grey; }
+.content-section {margin-bottom:2rem; display:flex; margin:10px; flex-direction:column; align-items:center; width:32%; border-radius:6px; background-color:#115614; height:55rem; color:aquamarine;}
 .content-section img{width:100%; height:auto; float:left;}
-.content-section h2:hover{color:green;}
+.content-section h2:hover{color:black;}
 .content-section p{}
-footer{ background-color:#333; color:#fff; text-align:center;padding:1rem; position:sticky; bottom:0;}
-.site-summary{background-color:#333; color:white; margin-bottom:2rem; margin:2%; padding:15px; width:94%; border-radius:6px; text-align:center}
+footer{ background-color:#800E3D; color:#fff; text-align:center;padding:1rem; position:sticky; bottom:0;}
+.site-summary{background-color:#800E3D; color:#53a334; margin-bottom:2rem; margin:2%; padding:15px; width:94%; border-radius:6px; text-align:center;}
 `
 createAndAppend("style", document.head,{},style)
